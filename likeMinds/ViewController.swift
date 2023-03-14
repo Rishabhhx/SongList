6fcedd80390fb9a4b25518f58941f845b279b500
Rishabh Sharma <rishabh.sharma@appinventiv.com>
2023-03-14 15:06:43 +0530

    Initial Commit

---
+++ likeMinds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likeMinds
+//
+//  Created by Rishabh Sharma on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
